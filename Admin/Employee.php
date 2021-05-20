aa15241b2ac7d5a79974bf1aa3d3c9a642108f6b
alyanasuhax <81115116+alyanasuhax@users.noreply.github.com>
2021-05-20 16:48:24 +0800

    Add files via upload

--- Admin/Employee.php
+++ Admin/Employee.php
@@ -8,7 +8,7 @@ include("../config.php");
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
-	<title>The Coders Admin Page</title>
+	<title>The Coders Online Grocery System</title>
 	
 	<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
 
@@ -55,96 +55,14 @@ include("../config.php");
 
 <body>
 
-<div id="container">
-
-	  
-	  
-	   <div id="header">
- 
-         
-		<div id="logo-banner">
-		   
-				<div id="logo">
-					<a href="index.php"><img src="images/logo.png" alt="" /></a>
-				</div>
-				
-				<div id="banner">
-                
-				</div>
-		
-		</div>
-		</div>
-		
+    <?php
+        include ("./dashboard.php")
+    ?>
 	
-	<div id="content-wrap">	
-	
-	<section id="secondary_bar">
-
-             <nav><!-- Defining the navigation menu -->
-                <ul>
-                    <li class="selected"><a href="index.php">Home</a></li>
-                    <li><a href="add_warehouse.php">Add warehouse</a></li>
-                    <li><a href="add_product.php">Add product</a></li>
-                    <li><a href="Employee.php">Add employee</a></li>
-                    <li><a href="add_category.php">Categories</a></li>
-                    <li class="logout"> <span class="check"> <?php echo "Welcome Mr/Miss:   ". "<font color='##fa5400'><i><b>".$login_session."</b></i></font>" ;?> </span></li>
-					
-                </ul>
-				
-            </nav>
-				
-            </nav>
-
-	</section><!-- end of secondary bar -->
-	   	
-<aside id="sidebar" class="column">
-			<!-- Begin Search -->
-				<div id="search">
-					<form action="searchemplo.php" method="post" accept-charset="utf-8">
-						<input type="text"  title="Search..." class="blink field"  placeholder="Search Employee"  name='search' size=60 maxlength=100/>
-						<input class="search-button" type="submit" value="Submit" />
-						<div class="cl">&nbsp;</div>
-					</form>
-					
-				</div>
-				<!-- End Search -->
-		<hr/>
-
-		
-		<h3>Reports:</h3>
-		<ul class="toggle">
-		    <li class="icn_settings"><a href="OrderReports.php">Order Report</a></li>
-			<li class="icn_settings"><a href="EmployeeReport.php">Employee Report</a></li>
-			<li class="icn_settings"><a href="CustomerReport.php">Customer Report</a></li>
-			<li class="icn_settings"><a href="ProductReport.php"> Product Report</a></li>
-     		
-		</ul>
-
-
-		
-		<h3>Administrator:</h3>
-		<ul class="toggle">
-		    <li class="icn_add_user"><a href="Employee.php">Add Employee</a></li>
-			<li class="icn_photo"><a href="add_product.php">Add Product</a></li>
-			<li class="icn_new_article"><a href="add_category.php">Add Category</a></li>
-		</ul>
-		
-        <h3>Tables:</h3>
-		<ul class="toggle">
-		    <li class="icn_categories"><a href="order.php">Order Detial</a></li>
-     		<li class="icn_categories"><a href="customerTable.php">Customer Detail</a></li>
-		</ul>
-
-		<h3>Admin</h3>
-		<ul class="toggle">
-			<li class="icn_jump_back"><a href="../logout.php">Logout</a></li>
-		</ul>
-	
-	</aside><!-- end of sidebar -->
 	
 	<section id="main" class="column">
 	
-	<h4 class="alert_info">Welcome To <strong>"SomStore"</strong> Admin Panel As: <?php echo "  ". "<font color='#f90'><big><b>".$login_session."</b></big></font>" ;?>  </h4>
+	<h4 class="alert_info">Welcome To <strong>"The Coders System Management"</strong> Admin Panel As: <?php echo "  ". "<font color='#f90'><big><b>".$login_session."</b></big></font>" ;?>  </h4>
 
 <script type="text/javascript">
 function validateForm()
@@ -186,74 +104,46 @@ if (c==null || c=="")
             });
         });
     </script>
-		
-
-				
 				
 	<div id="form_wrapper" class="form_wrapper">
-	
-		 <table>
-					<form class="register active" id="myForm"action="empRegistration.php"  method="POST" >
-
-					<th colspan="3"><h2>Register</h2> </th> 
-						
-
+		<table>
+			<form class="register active" id="myForm"action="empRegistration.php"  method="POST" >
+					<th colspan="3"><h2>Employee Information</h2> </th> 	
         <tr>
-       <td>  
-
-	  <label>Full Name:</label>
-		
-		<input type="text" id="empValid" name="fullname" placeholder="Full name" />
-		<span class="error">This is an error</span>
-	  </td>
-      <td>   
-
-	     <label>Username:</label>
-
-		<input type="text" id="username" name="username" placeholder="User name" />
-		<span class="error">This is an error</span>
-      </td>
+			<td>  
+				<label>Full Name</label>
+					<input type="text" id="empValid" name="fullname" placeholder="Full name" />
+					<span class="error">This is an error</span>
+			</td>
+			<td>   
+				<label>Username</label>
+					<input type="text" id="username" name="username" placeholder="User name" />
+					<span class="error">This is an error</span>
+			</td>
 		</tr>
-    <tr>
-         <td>  
-
-		<label> Selelct Employee image :</label>
-		<input type="file" name="picture" id="picture"  required>
-		<span id="pass-info"> </span>
-                               
-	    </td>
+		<tr>
+			<td>  
+				<label> Selelct Employee image :</label>
+					<input type="file" name="picture" id="picture"  required>
+					<span id="pass-info"> </span>          
+			</td>
 		
-       <td>  
-
-		<label> Enter Password:</label>
+			<td>  
+				<label> Enter Password:</label>
+					<input type="password" id="password" name="password" placeholder="*****" >
+					<span id="pass-info"> </span>				   
+			</td>
+		</tr>
 		
-		<input type="password" id="password" name="password" placeholder="*****" >
-		<span id="pass-info"> </span>
-                               
-	</td>
-
-   </tr>
-
-
-
-						<div class="bottom">
-
-						<td colspan="3">	
-						
-						<button name="submit" id="submit" title="Click to Save"  class="a-btn"> <span class="a-btn-text"> Add Employee</span></button>
-						
-						</td>
-							
-							<div class="clear"></div>
-						</div>
-						
-		
-
-	</form>
-					
-	</table>
-	</div>
-
+		<div class="bottom">
+			<td colspan="3">	
+				<button name="submit" id="submit" title="Click to Save"  class="a-btn"> <span class="a-btn-text"> Add Employee</span></button>		
+			</td>			
+			<div class="clear"></div>
+		</div>
+	</form>			
+</table>
+</div>
 
 						<script>
 <script type="text/javascript">
@@ -275,11 +165,6 @@ $(document).ready(function(){
 
 </script>
 		
-	
-
-
-
-
 
 <?php
 $result = mysqli_query($mysqli,"SELECT * FROM employee");
@@ -288,42 +173,40 @@ $result = mysqli_query($mysqli,"SELECT * FROM employee");
   <table class="tablesorter" cellspacing="0"> 
 
 			
-			<thead><tr> <th colspan="7"> Employee Data Record</th>  </tr> <thead>
+			<thead><tr> <th colspan="7">Employee Data Record</th>  </tr> <thead>
 		<thead>
 			<tr>
-   		    <th>Check</th> 
-    	      <th>Employee ID</th>
-              <th> Employee Name</th>			  
-    		<th>User Name</th>
-		    <th>Password</th>
-           <th>Picture</th>			
-    		<th>Actions</th>
+				<th>Check</th> 
+				<th>Employee ID</th>
+				<th>Employee Name</th>			  
+				<th>User Name</th>
+				<th>Password</th>
+				<th>Picture</th>			
+				<th>Actions</th>
 			</tr>
 		</thead>
 		<tbody>
 <?php while($row = mysqli_fetch_array($result))
   {?>
 
-      <tr>
-    <td><input type="checkbox"></td>
-    <td><?Php echo $row['Employee_ID']; ?></td>
-    <td><?php echo $row['Employee_Name']; ?></td>
-   <td><?php echo $row['Username']; ?></td>
-	<td><?php echo $row['Password']; ?></td>
-	<td><img src="../images/<?php echo $row['Picture']; ?>" width="40" height="40"></td>
-    <td> <a href="empViewUpdate.php?update=<?php echo $row['Employee_ID']; ?>"  onClick="edit(this);" title="empEdit" >  <input type="image" src="images/icn_edit.png" title="Edit"> </a>
-     <a href="empDelete.php?delete=<?php echo $row['Employee_ID']; ?>" onClick="del(this);" title="Delete"><input type="image" src="images/icn_trash.png" title="Trash"/>  </a></td>
-    </tr>
+		<tr>
+			<td><input type="checkbox"></td>
+			<td><?Php echo $row['Employee_ID']; ?></td>
+			<td><?php echo $row['Employee_Name']; ?></td>
+			<td><?php echo $row['Username']; ?></td>
+			<td><?php echo $row['Password']; ?></td>
+			<td><img src="../images/<?php echo $row['Picture']; ?>" width="40" height="40"></td>
+			<td> <a href="empViewUpdate.php?update=<?php echo $row['Employee_ID']; ?>"  onClick="edit(this);" title="empEdit" >  <input type="image" src="images/icn_edit.png" title="Edit"> </a>
+			 <a href="empDelete.php?delete=<?php echo $row['Employee_ID']; ?>" onClick="del(this);" title="Delete"><input type="image" src="images/icn_trash.png" title="Trash"/>  </a></td>
+		</tr>
 
   <?php }mysqli_close($mysqli);?>
       </tbody>
-</table>
-	  
- </div> 
-
+</table>  
+</div> 
 	</section>
-          </div>
-   </div>
+    </div>
+</div>
     
 </body>
 
---
+++ Admin/empViewUpdate.php
@@ -0,0 +1,121 @@
+	<?php 
+	include("../config.php"); 
+   include("../session.php");
+	?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>The Coders Online Grocery System</title>
+	
+	<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
+	<!--[if lt IE 9]>
+	<link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" />
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script src="js/jquery-1.5.2.min.js" type="text/javascript"></script>
+	<script src="js/hideshow.js" type="text/javascript"></script>
+	<script src="js/jquery.tablesorter.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.equalHeight.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() 
+    	{ 
+      	  $(".tablesorter").tablesorter(); 
+   	 } 
+	);
+	$(document).ready(function() {
+
+	//When page loads...
+	$(".tab_content").hide(); //Hide all content
+	$("ul.tabs li:first").addClass("active").show(); //Activate first tab
+	$(".tab_content:first").show(); //Show first tab content
+
+	//On Click Event
+	$("ul.tabs li").click(function() {
+
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_content").hide(); //Hide all tab content
+
+		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active ID content
+		return false;
+	});
+
+});
+    </script>
+    <script type="text/javascript">
+    $(function(){
+        $('.column').equalHeight();
+    });
+</script>
+
+</head>
+
+
+<body>
+
+    <?php
+        include ("./dashboard.php")
+    ?>
+	
+<section id="main" class="column">
+
+<?php
+$update=$_GET['update'];
+$result = mysqli_query($mysqli,"SELECT * FROM employee where Employee_ID ='$update'");
+?>
+<?php if($row = mysqli_fetch_array($result))
+  {?> 
+  
+	
+<div id="form_wrapper" class="form_wrapper">
+	<table>
+	<form class="register active" action="empUpdate.php" method="POST" autocomplete="off">
+	<th colspan="3"><h2>Employee</h2> </th> 
+		
+	<tr>
+		<td>
+			<input type="hidden" id="ID" name="ID" value="<?php echo $row['Employee_ID'];?>"  placeholder="Full name" required>
+			<span class="error">This is an error</span>
+		</td>
+ 
+		<td>  
+			<label>Full Name</label>
+			<input type="text" id="name" name="name" value="<?php echo $row['Employee_Name'];?>"  placeholder="Full name" required>
+				<span class="error">This is an error</span>
+		</td>
+		<td>   
+			<label>Username</label>
+				<input type="text" id="username" name="username" value="<?php echo $row['Username'];?>" placeholder="User name" required>
+				<span class="error">This is an error</span>
+		</td>
+		<td>  
+			<label>Enter Password</label>
+				<input type="password" id="password" name="password" value="<?php echo $row['Password'];?>" placeholder="*******" required>
+				<span id="pass-info"> </span>                 
+		</td>
+   </tr>
+   
+    <tr>
+			<div class="bottom">
+				<td colspan="3">	
+					<button type="submit"  name="submit" value="Update" class="a-btn"> <span class="a-btn-text">Update Employee</span></button>	
+				</td>			
+			<div class="clear"></div>
+		</div>
+	</tr>
+	</form>
+					
+	</table>
+	</div>
+         <?php }?>
+		
+	</section>
+          </div>
+   </div>
+    
+</body>
+
+</html>
